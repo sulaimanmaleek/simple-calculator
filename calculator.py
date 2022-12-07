eb2a9976646831e4df92c47a78ea0eb6344cece5
sulaimanmaleek <sulaimanmaleek2022@gmail.com>
2022-12-06 16:41:05 -0800

    Calculator with python

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print('_'*75)
+print('>>>>>>>>>>>>>>>>>>>>>>>>I AM A SIMPLE CALCULATOR<<<<<<<<<<<<<<<<<<<<<')
+print('_'*75)
+
+a= int (input ("ENTER FIRST VALUE:"))
+b= int (input("ENTER SECOND VALUE:"))
+
+w=a+b
+x=a-b
+y=a*b
+z=a/b
+
+print('This is a simple calculation for the operation between', a , 'and', b)
+print("Addition:",w)
+print("Subtraction:",x)
+print("Multiplication:",y)
+print("Division:",z)
+
+
+print('_'*75)
+print('>>>>>>>>>>>>>>>>>>>>>>>>I AM A SIMPLE CALCULATOR<<<<<<<<<<<<<<<<<<<<<')
+print('_'*75)
+
+
+
